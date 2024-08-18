f4cca19df234dbfb5113c6f8f9cbf903be5de28a
keithzano <keithzano@gmail.com>
2024-08-18 23:40:41 +0200

    Adds the section the the home pages so that they can be rendered

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
 import Image from "next/image";
 import { Hero } from "./components/Hero";
+import { ImprovementSection } from "./components/ImprovementSection";
+import { NewsSection } from "./components/NewsSection";
+import { NewsletterSection } from "./components/NewsletterSection";
 
 export default function Home() {
   return (
     <main>
       <Hero/>
+      <ImprovementSection/>
+      <NewsSection/> 
+      <NewsletterSection/>
     </main>
   );
 }
